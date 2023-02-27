85834d15c87b2b36700c6f28a023e18fdb28a47b
PuzanovAlexander <125386108+PuzanovAlexander@users.noreply.github.com>
2023-02-27 19:54:17 +0300

    Delete lab_practic3.py

--- lab_practic3.py
+++
@@ -1,6 +0,0 @@
-year = int(input("Введите год: "))
-
-if year % 4 == 0 & (year % 100) % 4 == 0:
-    print("Год " + str(year) + " високосный")
-else:
-    print("Год " + str(year) + " не високосный")
